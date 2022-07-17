d6e16739b1eabdd6a087bb07d58fdac477def53a
Ehmaed Alhams <ehmaedalzhar@gmail.com>
2022-07-17 14:13:40 +0300

    feat: solve some problem.

--- js/main.js
+++ js/main.js
@@ -7,4 +7,27 @@
 // document.querySelector("h1").style = "color: blue; font-size: 40px;"
 
 // Challenge 3:
-console.log("%cElzero %cWeb %cSchools", "font-size: 40px;color: red","color: green;font-size: 40px;", "background-color: blue;font-size: 40px;");
+// console.log("%cElzero %cWeb %cSchools", "font-size: 40px;color: red","color: green;font-size: 40px;", "background-color: blue;font-size: 40px;");
+
+
+//Challenge 4:
+// console.log("This is the outer level");
+// console.group("level 1");
+// console.log("This is level 2");
+// console.group("level 2");
+// console.log("Level 3");
+
+
+//Challenge 5:
+// console.table(["Sara", "Elzero", "Gamal", "Ahmad", "Sameh", "Ehmaed"])
+
+//Challenge 6:
+// var numberOne = 10;
+// var numberTwo = 20;
+
+// console.log(typeof numberOne);
+// console.log(typeof numberTwo);
+
+// // Template Template:
+// console.log(`${numberOne}${numberTwo}`);
+// console.log(typeof `${numberOne}${numberTwo}`);
