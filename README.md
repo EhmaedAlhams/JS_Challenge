34f4fb19c2d793f6615a0bc8fb3525a47cb57c32
Ehmaed Alhams <ehmaedalzhar@gmail.com>
2022-07-17 12:19:16 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS_Challenge
+ problem solving js with elzero
---
+++ main.js
@@ -0,0 +1,5 @@
+document.write("<h1>Elzero</h1>")
+
+document.querySelector("h1").style.fontWeight = "bold";
+document.querySelector("h1").style.textAlign = "center";
+document.querySelector("h1").style = "color: blue; font-size: 40px;"
