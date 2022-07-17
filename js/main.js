bf013807242f15c1b5ed2037a929feb254cc6df8
Ehmaed Alhams <ehmaedalzhar@gmail.com>
2022-07-17 13:31:19 +0300

    update: solve challenge 3.
    solve challenge 3 with js.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Challenge 2:
+
+// document.write("<h1>Elzero</h1>")
+
+// document.querySelector("h1").style.fontWeight = "bold";
+// document.querySelector("h1").style.textAlign = "center";
+// document.querySelector("h1").style = "color: blue; font-size: 40px;"
+
+// Challenge 3:
+console.log("%cElzero %cWeb %cSchools", "font-size: 40px;color: red","color: green;font-size: 40px;", "background-color: blue;font-size: 40px;");
